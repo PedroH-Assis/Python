3a2a4bcb4d4d0f06ebf0df92e9dc967681e67fd0
Pedro <pedrohenriqueassis90@gmail.com>
2025-10-14 13:47:15 -0300

    print

---
+++ print.py
@@ -0,0 +1,15 @@
+print("Testes de argumentos em Python0")
+
+print("Testes de ar\ngumentossss em Python1")
+
+print("Testes" , "de argumentos" , "em Python2")
+
+print("Testes de argumentos em Python3" , end="")
+
+print(" Testes de argumentos em Python3.1")
+
+print("Testes" , "de argumentos" , "em Python4" , sep="-")
+
+print("Testes de argumentos em Python5" , sep="_", end="*\n")
+
+print(" Testes de argumentos em Python5.1", sep="")
