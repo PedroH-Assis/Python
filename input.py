9fdaff2f767ee2c066a16cd42691f026aaac54ed
Pedro <pedrohenriqueassis90@gmail.com>
2025-10-16 12:17:32 -0300

    input

---
+++ input.py
@@ -0,0 +1,63 @@
+# anything = float(input("Digite um número: "))
+
+# something = anything ** 2.0
+
+# print(anything, "elevado a 2 é", something)
+
+# anything2 = input ("Digite um número: ")
+
+# print(anything2 , "é um numero")
+
+# lado_a = float(input("Insira o comprimento da primeira perna: "))
+
+# lado_b = float(input("Insira o comprimento da segunda perna: "))
+
+# hipo = (lado_a ** 2 + lado_b ** 2) ** .5
+
+# print ("O comprimento da hiptenusa é:" , hipo)
+
+# lado_a = float(input("Insira o comprimento da primeira perna: "))
+
+# lado_b = float(input("Insira o comprimento da segunda perna: "))
+
+# print ("O comprimento da hiptenusa é:" , (lado_a ** 2 + lado_b ** 2) ** .5)
+
+# pnome = input("Posso ter seu primeiro nome, por favor? ")
+
+# fnome = input("Posso ter seu sobrenome, por favor? ")
+
+# print ("Obrigado!.")
+
+# print("\nSeu nome é " + pnome + " " + fnome + ".")
+
+# print("+" + 10 * "-" + "+")
+
+# print(("|" + " " * 10 + "|\n") * 5, end="")
+
+# print("+" + 10 * "-" + "+")
+
+# str (number) converter numero em string
+
+
+
+# lado_a = float(input("Insira o comprimento da primeira perna: "))
+
+# lado_b = float(input("Insira o comprimento da segunda perna: "))
+
+# print ("O comprimento da hiptenusa é:" , str((lado_a ** 2 + lado_b ** 2) ** .5))
+
+valor_1 = float(input("Digite o valor 1: "))
+
+valor_2 = float(input("Digite o valor 2: "))
+
+print("O resultado da adição é: " , (valor_1 + valor_2))
+
+print("O resultado da subtração é: " , (valor_1 - valor_2))
+
+print("O resultado da multiplicação é: " , (valor_1 * valor_2))
+
+print("O resultado da divisão é: " , (valor_1 / valor_2))
+
+print("\nIsso é tudo, pessoal!")
+
+result = flout
