5d08a80e75189b228a061e8a97f1a0216c11dce0
Pedro <pedrohenriqueassis90@gmail.com>
2025-10-16 09:35:11 -0300

    var

--- print.py
+++ print.py
@@ -13,3 +13,95 @@ print("Testes" , "de argumentos" , "em Python4" , sep="-")
 print("Testes de argumentos em Python5" , sep="_", end="*\n")
 
 print(" Testes de argumentos em Python5.1", sep="")
+
+print ("2")
+
+print (2)
+
+print (0o123)
+
+print (0x123)
+
+print (3E8)
+
+print (0.0000000000000000000001)
+
+print ("Eu gosto \"Monty Python\"")
+
+print('Eu gosto "Monty Python"')
+ 
+print(True > False)
+
+print(True < False)
+ 
+print ("\"Eu sou\"\n\"\"aprendizado\"\"\n\"\"\"Python\"\"\"")
+ 
+print(2+2)
+ 
+print(2 ** 3)
+
+print(2 ** 3.)
+
+print(2. ** 3)
+
+print(2. ** 3.)
+
+print(2 * 3)
+
+print(2 * 3.)
+
+print(2. * 3)
+
+print(2. * 3.)
+
+print(6 / 3)
+
+print(6 / 3.)
+
+print(6. / 3)
+
+print(6. / 3.)
+
+print(6 // 3)
+
+print(6 // 3.)
+
+print(6. // 3)
+
+print(6. // 3.)
+
+print(6 // 4)
+
+print(6. // 4)
+
+print(14 % 4)
+
+print(12 % 4.5)
+ 
+print(-4 + 4)
+
+print(-4. + 8)
+
+print(-4 - 4)
+
+print(4. - 8)
+
+print(-1.1)
+
+print(+2)
+
+print(9 % 6 % 2)
+ 
+print(2 ** 2 ** 3)
+
+print(-3 ** 2)
+
+print(-2 ** 3)
+
+print(-(3 ** 2))
+
+print(2 * 3 % 5)
+
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+ 
+ 
---
+++ var.py
@@ -0,0 +1,65 @@
+var = 1
+
+print (var)
+
+var2 = 1
+
+account_name = 'John Doe'
+
+account_balance = 1000.0
+
+print (var, account_balance, account_name)
+
+var3 = "3.8.5"
+
+print ("Versão do Python " + var3)
+
+var4 = 1
+
+print(var4)
+
+var4 = var4 + 1
+
+print(var4)
+
+var5 = 100
+
+var5 = 200+300
+
+print(var5)
+
+a = 3.0
+
+b = 4.0
+
+c = (a ** 2 + b ** 2) ** 0.5
+
+print ("c =" , c)
+
+John = 3
+
+Maria = 5
+
+Adam = 6    
+
+print (John, Maria, Adam, sep= ',')
+
+total_apples = John + Maria + Adam
+
+print(total_apples)
+
+sheep = 1
+
+sheep = sheep * 2
+
+sheep *=2
+
+print (sheep)
+
+x = 1
+
+x = x + 2
+
+x += 2
+
+print (x)   
