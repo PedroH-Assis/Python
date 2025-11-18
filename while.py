328e297d5d9c7023bb972b9d3eba2bee81116007
Pedro <pedrohenriqueassis90@gmail.com>
2025-11-18 13:46:59 -0300

    while

---
+++ while.py
@@ -0,0 +1,54 @@
+# while True:
+#     print("Loop eterno")
+
+# maior_numero = -99999 
+# num = int(input("Digite um numero ou digite -1 para parar "))
+
+# while num != -1:
+
+#     if num > maior_numero:
+
+#         maior_numero = num
+
+#     num = int(input("Digite um numero ou digite -1 para parar "))
+
+
+
+# print("O maior numero é:" , maior_numero)
+
+
+# impar_num = 0
+
+# par_num = 0
+
+# num = int(input("Digite um número ou digite 0 para parar: "))
+
+# while num != 0:
+
+#     if num % 2 == 1:
+
+#         impar_num += 1
+
+#     else:                   #Sempre prestar atenção na identação do código para as funções funcionarem
+
+#         par_num += 1
+
+#     num = int(input("Digite um número ou digite 0 para parar: "))
+
+
+
+# print ("Números ímpares contam:" , impar_num)
+# print ("Números pares contam:" , par_num)
+
+
+# contador = 5
+
+# while contador != 0:
+
+#     print("Dentro do laço." , contador)
+
+#     contador -= 1
+
+# print("Fora do circuito." , contador)
+
+ 
