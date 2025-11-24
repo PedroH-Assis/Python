aa58f41363eceb22773ffb92bd850d1153489293
Pedro <pedrohenriqueassis90@gmail.com>
2025-11-24 17:16:49 -0300

    for_2

---
+++ for.py
@@ -0,0 +1,64 @@
+# for i in range(10):
+#    print("O valor de i é atualmente", i)
+
+# for i in range(2, 8):
+#     print("O valor de i é atualmente", i)
+
+# power = 1
+# for expo in range(16):
+#   print("2 à potência de", expo, "é", power)
+#   power *= 2
+
+# import time
+
+# for i in range(1, 6):
+
+#     print(i , "Mississippi")
+#     time.sleep(1)
+
+# print("Pronto ou não, aqui vou eu!!")
+
+# print("The break instrução:")
+# for i in range(1, 6):
+#     if i == 3:
+#         break      Sai totalmente do loop
+#     print("Dentro do laço.", i)
+# print("Fora do loop.")
+
+
+# # continue - exemplo
+
+# print("The continue instrução:")
+# for i in range(1, 6):
+#     if i == 3:
+#         continue         Pula a iteração atual, mas continua o loop
+#     print("Dentro do laço.", i)
+# print("Fora do loop.")
+
+# largest_number = -99999999
+# counter = 0
+
+# number = int(input("Insira um número ou digite -1 para finalizar o programa: "))
+
+# while number != -1:
+#     if number == -1:
+#         continue
+#     counter += 1
+
+#     if number > largest_number:
+#         largest_number = number
+#     number = int(input("Insira um número ou digite -1 para finalizar o programa: "))
+
+# if counter:
+#     print("O maior número é",  largest_number)
+# else:
+#     print("Você nnão tem inseriu qualquer número.")
+
+# while True:
+#     palavra = input("Digite uma palavra: ")
+
+#     if palavra == "chupacabra":
+#         print("Você saiu do loop com sucesso")
+#         break
+
+
